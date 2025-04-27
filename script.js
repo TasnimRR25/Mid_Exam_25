f181cc5292a82c099d6223fb27fbe716580c00f4
TasnimRR25 <tasnimrahmanrafia0@gmail.com>
2025-04-27 13:41:13 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function sayHello() {
+    alert("👋 Hello! Welcome to Munna's Store!");
+}
+
+function buyProduct() {
+    alert("🛒 Thanks for your purchase!");
+}
+
+function thankUser() {
+    alert("✅ Thanks for contacting us! We'll get back to you soon.");
+}
